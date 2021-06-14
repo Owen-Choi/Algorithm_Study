fead8796577df0f17a5aab5cf13e57da6451e489
Owen-Choi <oldstyle482@gmail.com>
2021-06-14 15:07:59 +0900

    자바 복습 1 : linear search

---
+++ src/Linear_Search.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Linear_Search {
+
+    public static void main(String[] args) {
+        int[] arr; int length;
+        int i;
+        int target;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the length of the array");
+        length = sc.nextInt();
+        arr = new int[length];
+        for(i=0; i<arr.length; i++){
+            System.out.println("Enter " + i + "th number to array");
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Please Enter the Target number : ");
+        target = sc.nextInt();
+        for(i=0; i<arr.length; i++){
+            if(arr[i] == target){
+                System.out.println("Target Found");
+                return;
+            }
+        }
+        System.out.println("Target not found");
+        return;
+    }
+}
