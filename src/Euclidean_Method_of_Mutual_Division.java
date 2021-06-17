3a69d5f5dcfcad5bf3365164eb3d27f6c7712996
Owen-Choi <oldstyle482@gmail.com>
2021-06-17 13:48:44 +0900

    자바 복습 6: 재귀 : 팩토리얼(Recursive Factorial) 자바 복습 7: 재귀 : 유클리드 호제법(Recursive Euclidean Method of Mutual Division)

---
+++ src/Euclidean_Method_of_Mutual_Division.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Euclidean_Method_of_Mutual_Division {
+
+    int x;  int y;
+
+    public int GCD(int x, int y){
+        if(y == 0)
+            return x;           //말이 x지 사실 x의 위치에 있는 값은 y이다. y에는 y%x, 즉 0이 들어간다.
+        else
+            return GCD(y, x%y);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int xVal;   int yVal;   int result;
+
+        System.out.println("Please Enter x value : ");
+        xVal = sc.nextInt();
+        System.out.println("Please Enter y value : ");
+        yVal = sc.nextInt();
+        Euclidean_Method_of_Mutual_Division Euclid = new Euclidean_Method_of_Mutual_Division();
+        result = Euclid.GCD(xVal, yVal);
+
+        System.out.println("The Greatest Common Divisor(GCD) is " + result);
+    }
+}
---
+++ src/Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    int num;
+
+
+    public int Fac(int num){
+        if(num > 0)
+            return num * Fac(num - 1);
+        else
+            return 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int val;
+        System.out.println("Please Enter value : ");
+        val = sc.nextInt();
+        Factorial Fc = new Factorial();
+
+        System.out.println(Fc.Fac(val));
+    }
+}
