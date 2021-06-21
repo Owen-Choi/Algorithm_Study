1401e5909a8e12129186abff09d13f288cdd2bc0
Owen-Choi <oldstyle482@gmail.com>
2021-06-21 15:34:35 +0900

    자바 복습 19: 병합 정렬(Merge Sort)

---
+++ src/Java_Algorithms/Merge_Sort.java
@@ -0,0 +1,34 @@
+package Java_Algorithms;
+
+class Merge_Sort {
+    static int[] Merge(int[] Aarr, int[] Barr){
+        int[] arr = new int[Aarr.length + Barr.length];
+        int pa,pb,pc;           //3가지 배열의 포인터들
+        pa = pb = pc = 0;
+        while(pa <= Aarr.length-1 || pb <= Barr.length - 1){
+
+            while(pa < Aarr.length && pb < Barr.length){
+                arr[pc++] = (Aarr[pa] <= Barr[pb]) ? Aarr[pa++] : Barr[pb++];
+            }
+            while(pa < Aarr.length){
+                arr[pc++] = Aarr[pa++];
+            }
+            while(pb < Barr.length){
+                arr[pc++] = Barr[pb++];
+            }
+        }
+        return arr;
+    }
+
+    static void print(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.printf("%3d", arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] Aarr = {1,3,5,7,9};
+        int[] Barr = {2,4,6,8,10};
+        print(Merge(Aarr, Barr));
+    }
+}
