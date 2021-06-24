bb4930c20ba2ef02995be5b76a9de943162042d1
Owen-Choi <oldstyle482@gmail.com>
2021-06-24 17:45:32 +0900

    자바 공부 26 : Boyer-Moore

---
+++ src/Java_Algorithms/Boyer_Moore.java
@@ -0,0 +1,34 @@
+package Java_Algorithms;
+
+class Boyer_Moore {
+    static boolean BM(String Sentence, String Pattern){
+        int[] skip = new int[Character.MAX_VALUE + 1];
+        int i;
+        for(i=0; i<=Character.MAX_VALUE; i++){
+            skip[i] = Pattern.length();
+        }
+        for(i=0; i<Pattern.length()-1; i++){
+            // 이렇게도 쓸 수 있구나...
+            skip[Pattern.charAt(i)] = Pattern.length() - i - 1;
+        }
+        int SP,PP;
+        SP = Pattern.length() - 1;
+        // 패턴에 문자가 있는지 먼저 검사, 있다면 패턴을 얼만큼 옮길지 결정
+        while(SP < Sentence.length()){
+            PP = Pattern.length() - 1;
+            while(Sentence.charAt(SP) == Pattern.charAt(PP)){
+                if(PP == 0)
+                    return true;
+                SP--;
+                PP--;
+            }
+            SP += Math.max(skip[Sentence.charAt(SP)], Pattern.length() - PP);
+            // 위와 같이 비교하는 이유는 패턴 내에 반복되는 문자가 있는 경우 때문이다.
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BM("ABCXDEZCABACABAC", "ABAC"));
+    }
+}
